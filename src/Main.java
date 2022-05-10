451147ea878ae69e1f1f1a3ae4aa97e68e2b50f5
KayaraSilveira <silveira.kayara@gmail.com>
2022-05-10 20:32:32 -0300

    registradores

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ public class Main {
 
     public static void main(String[] args) {
         boolean sera;
+        int hm;
         Flags batata = new Flags(10, 0, 1, 0, 1);
-        sera = batata.isIndirect();
+        batata.set_b(0);
+        batata.set_e(0);
+        sera = batata.isAbsolute();
+        hm = batata.get_ni();
         System.out.println(sera);
+        System.out.println(hm);
     }
 }
