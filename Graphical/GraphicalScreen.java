3eb703b722d2a7677cf46d40ad54e9ef792a3758
Kathe Isabelle <kathe.isabelle@inf.ufpel.edu.br>
2022-05-12 20:23:54 -0300

    graphic

---
+++ Graphical/GraphicalScreen.java
@@ -0,0 +1,49 @@
+package PS;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GraphicalScreen extends JFrame {
+    private JTextField TFvalor1;
+    private JTextField TFvalor02;
+    private JButton btnRUN;
+    private JButton bntSTEP;
+    private JPanel mainPanel;
+    private JLabel VMtittle;
+
+    public GraphicalScreen(){
+        setContentPane(mainPanel);
+        setTitle("Virtual Machine SIC-HEX");
+        setSize(500,500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+        //AÇÕES BOTAO RUN
+        btnRUN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String run = TFvalor1.getText();
+
+
+            }
+        });
+
+        //AÇÕES BOTAO STEP
+        bntSTEP.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+    }
+
+    public static void main(String[]args){
+        GraphicalScreen myFrame = new GraphicalScreen();
+    }
+
+
+
+}
