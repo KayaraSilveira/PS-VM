3d95e63f30a1a0201cff3410319e9a39703a8783
Rafael Copes <rafaelcopes16@gmail.com>
2022-05-15 22:44:06 -0300

    loader que não funciona

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
+        CPU cpu = new CPU();
 
-    int a;
+        try {
+            Loader.load(cpu, "src/test.obj");
+        } catch (FileNotFoundException e1) {
+            System.out.println(e1);
+            return;
+        }
 
     }
 }
