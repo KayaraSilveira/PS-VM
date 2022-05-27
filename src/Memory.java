d4ec50f5a33ef5469390acf65181a542d6cb30c7
Rafael Copes <rafaelcopes16@gmail.com>
2022-05-27 20:56:22 -0300

    mundanças no setWord e add instructionCount

--- src/Memory.java
+++ src/Memory.java
@@ -2,67 +2,95 @@ import java.util.Arrays;
 
 public class Memory{
 
-    public final String[][] memoria;
+    public final String[][] memory;
+    private int instructionCount = 0;
+
+    public void incInstructionCount() {
+        this.instructionCount++;
+    }
+    public int getInstructionCount() {
+        return this.instructionCount;
+    }
 
 
     public Memory(){
-        this.memoria = new String[1024][8];
+        this.memory = new String[1024][8];
     }
 
     public void reset(){
-        Arrays.fill(memoria, "0");
+        Arrays.fill(memory, "0");
     }
 
-    public boolean confereEndereco(String endereco){
-        int enderecoInt;
-        enderecoInt = Conversion.stringBinaryToInt(endereco);
-        boolean verifica = enderecoInt < 0 || enderecoInt >= 1024;
-        if(verifica){
+    public boolean checkAddress(String address){
+        int addressInt;
+        addressInt = Conversion.stringBinaryToInt(address);
+        boolean check = addressInt < 0 || addressInt >= 1024;
+        if(check){
             System.out.println("Endereço de memória é inválido.");
         }
-        return verifica;
+        return check;
     }
 
     public String getByte(String address) {
         int addressInt, i;
         String byteReturn = "";
-        confereEndereco(address);
+        checkAddress(address);
         addressInt = Conversion.stringBinaryToInt(address);
         for(i = 0; i < 8; i++) {
-            byteReturn =  byteReturn + memoria[addressInt][i];
+            byteReturn =  byteReturn + memory[addressInt][i];
         }
         return byteReturn;
     }
 
     public void setByte(String address, String value) {
-        if (confereEndereco(address)) {
+        if (checkAddress(address)) {
+            System.out.println("endereço inválido!");
             return;
         }
         int addressInt, i;
         addressInt = Conversion.stringBinaryToInt(address);
+        //System.out.println(addressInt);
+        //System.out.println(value);
+
         for(i = 0; i < 8; i++) {
-            memoria[addressInt][i] =  value.substring(0 + i, 0 + i + 1);
+            memory[addressInt][i] =  value.substring(0 + i, 0 + i + 1);
         }
     }
     public String getWord(String address) {
         int addressInt, i,j;
         String byteReturn = "";
         addressInt = Conversion.stringBinaryToInt(address);
-        for (j=0; j<3; j++) {
+        for (j = 0; j < 3; j++) {
             for (i = 0; i < 8; i++) {
-                byteReturn = byteReturn + memoria[addressInt][i];
+                byteReturn = byteReturn + memory[addressInt][i];
             }
         }
         return byteReturn;
     }
 
-    public void setWord(String address, String value){
+    public void setWord(String address, String value) {
+        /*
+        System.out.println(value);
+        System.out.println(address);
+        System.out.println(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+1));
+        System.out.println(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+2));
+        System.out.println(value.substring(0, 8));
+        System.out.println(value.substring(8, 16));
+        System.out.println(value.substring(16, 24));*/
 
-        setByte(address, Conversion.ShiftR(value, 16));
-        setByte(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+1), Conversion.ShiftR(value, 8));
-        setByte(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+2), value);
+        //System.out.println(address);
+        //System.out.println(value);
+        //System.out.println(value.length());
+
+        if (value.length() == 24) {
+            setByte(address, value.substring(0, 8));
+            setByte(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+1), value.substring(8, 16));
+            setByte(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+2), value.substring(16, 24));
+        }
+
+        if (value.length() == 16) {
+            setByte(address, value.substring(0, 8));
+            setByte(Conversion.intToStringBinary(Conversion.stringBinaryToInt(address)+1), value.substring(8, 16));
+        }
     }
-
-
-
 }
